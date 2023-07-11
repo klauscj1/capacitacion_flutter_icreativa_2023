d8d6bdb61d9d74c506acd0a74aa1148b14703cfc
klauscj <clauschochoe@gmail.com>
2023-07-10 21:34:52 -0500

    clase 8

--- 02_biblioteca_widgets/lib/app/presentation/screens/onboarding_screen.dart
+++ 02_biblioteca_widgets/lib/app/presentation/screens/onboarding_screen.dart
@@ -1,9 +1,35 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:biblioteca_widgets/app/domain/models/slider_item_model.dart';
 import 'package:biblioteca_widgets/app/presentation/shared_widgets/slide.dart';
 import 'package:flutter/material.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  PageController pageViewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+    pageViewController.addListener(() {
+      final page = pageViewController.page ?? 0;
+      if (page >= (sliders.length - 1.5)) {
+        setState(() {
+          endReached = true;
+        });
+      } else {
+        setState(() {
+          endReached = false;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +37,7 @@ class OnboardingScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
-            //controller: PageController,
+            controller: pageViewController,
             physics: const BouncingScrollPhysics(),
             children: sliders
                 .map((slide) => SliderWidget(
@@ -29,20 +55,30 @@ class OnboardingScreen extends StatelessWidget {
               },
             ),
           ),
-          //TODO: Mostrar este boton solo cuando sea la ultima pagina
-          // controller dentro de PageView de tipo PageController, esto debe transformarse en un statefullWidget
           Positioned(
             bottom: 30,
             right: 30,
-            child: FilledButton(
-              child: const Text("Empezar"),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
+            child: endReached
+                ? FadeInRight(
+                    from: 15,
+                    delay: const Duration(milliseconds: 500),
+                    child: FilledButton(
+                      child: const Text("Empezar"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  )
+                : Container(),
           )
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
   }
 }
